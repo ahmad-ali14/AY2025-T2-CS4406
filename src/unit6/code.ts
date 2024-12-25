b2ea51689b355cbcaddf769afc92b5ccbc89ad15
Ahmad Ali <aallii300300@gmail.com>
2024-12-25 21:12:39 +0000

    unit6: add controls + comments

--- src/unit6/code.ts
+++ src/unit6/code.ts
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { createBaseScene } from "../utils/createBaseScene";
-import { createTextSprite } from "../utils/createTextSprite";
 
 const earthImageUrls = [
     "https://upload.wikimedia.org/wikipedia/commons/c/cf/WorldMap-A_non-Frame.png",
@@ -16,8 +15,6 @@ const {
     directionalLight,
     renderer,
     canvas,
-    camera,
-    shouldShowLabels,
     shouldShowWireframe,
     sidebar,
     addHelpNote,
@@ -35,6 +32,7 @@ const {
 
 // a base number that all other numbers will be relative to
 const bl = 100;
+// for easy access to the edges of the scene
 const sceneEdges = {
     top: canvas.height / 2,
     bottom: -canvas.height / 2,
@@ -42,18 +40,21 @@ const sceneEdges = {
     right: canvas.width / 2,
 };
 
+// earth: geometry + map + material
 const earthGeometry = new THREE.SphereGeometry(bl, 32, 32);
+const earthMap = new THREE.TextureLoader().load(earthImageUrls[0]!);
 const earthMaterial = new THREE.MeshStandardMaterial({
     wireframe: shouldShowWireframe(),
-    map: new THREE.TextureLoader().load(earthImageUrls[0]!),
+    map: earthMap,
 });
-
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 
+// moon: geometry + map + material
 const moonGeometry = new THREE.SphereGeometry(bl / 3, 32, 32);
+const moonMap = new THREE.TextureLoader().load(moonImageUrls[0]!);
 const moonMaterial = new THREE.MeshStandardMaterial({
     wireframe: shouldShowWireframe(),
-    map: new THREE.TextureLoader().load(moonImageUrls[0]!),
+    map: moonMap,
 });
 
 const moon = new THREE.Mesh(moonGeometry, moonMaterial);
@@ -61,18 +62,20 @@ moon.position.x = bl * 2; // move moon to the right of the earth
 
 scene.add(earth, moon);
 
+// rotate the earth around itself
 const rotateEarth = () => {
     earth.rotation.y += bl / 10000;
 };
 
+// rotate the moon around itself
 const rotateMoon = () => {
     moon.rotation.y += bl / 10000;
 };
 
-// move moon around earth
-const moveMoonAroundEarth = () => {
-    moon.position.x = bl * 2 * Math.cos(earth.rotation.y);
-    moon.position.z = bl * 2 * Math.sin(earth.rotation.y);
+// move moon around earth in a circular path
+const moveMoonAroundEarth = (speed = 1) => {
+    moon.position.x = bl * 2 * Math.cos(earth.rotation.y * speed);
+    moon.position.z = bl * 2 * Math.sin(earth.rotation.y * speed);
 };
 
 /**
@@ -100,6 +103,7 @@ earth.receiveShadow = true;
 moon.castShadow = true;
 moon.receiveShadow = true;
 
+// shadow camera properties
 const dlCameraFactor = 20;
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.near = bl * dlCameraFactor * 0;
@@ -109,12 +113,101 @@ directionalLight.shadow.camera.right = bl * dlCameraFactor;
 directionalLight.shadow.camera.top = bl * dlCameraFactor;
 directionalLight.shadow.camera.bottom = -bl * dlCameraFactor;
 
+// grouping materials for easy access
+const materials = [earthMaterial, moonMaterial];
+
+/**
+ * Scene Options:
+ * - Rotate: Start/Stop the moon rotating around the earth.
+ * - Rotation Speed: Adjust the speed of rotation. Default is 1.
+ * - Rotation Direction: Change the direction of rotation. Default is counter clockwise.
+ * - Rotation Axis: Change the axis of rotation, the shape will rotate around this axis.
+ */
+const sceneOptionsDiv = document.createElement("div");
+sceneOptionsDiv.classList.add("mb-4");
+
+sceneOptionsDiv.innerHTML = `
+    <hr class="border border-b-[#000] mb-2" />
+    <h2 class="text-xl font-bold text-center mb-2">Scene Options</h2>
+    <div class="flex flex-col space-y-2 text-lg">
+        <div>
+            <input type="checkbox" id="rotate" checked>
+            <label for="rotate" class="font-bold">Rotate</label>
+        </div>
+        <div>
+            <label for="rotationSpeed" class="font-bold">Rotation Speed</label>
+            <input type="range" id="rotationSpeed" min="0" max="3" step="0.01" value="0.01">
+        </div>
+        <div class="">
+            <label for="rotationDirection" class="font-bold">Rotation Direction</label>
+            <select id="rotationDirection">
+                <option value="1">Clockwise</option>
+                <option value="-1">Counter Clockwise</option>
+            </select>
+        </div>
+       
+    </div>
+    `;
+
+sidebar.appendChild(sceneOptionsDiv);
+
+// control listeners
+let shouldRotate = true;
+let rotationSpeed = 1;
+let rotationDirection = -1;
+
+const rotateCheckbox = document.getElementById("rotate") as HTMLInputElement;
+const rotationSpeedInput = document.getElementById(
+    "rotationSpeed",
+) as HTMLInputElement;
+const rotationDirectionSelect = document.getElementById(
+    "rotationDirection",
+) as HTMLSelectElement;
+
+rotateCheckbox.checked = shouldRotate;
+rotationSpeedInput.value = rotationSpeed.toString();
+rotationDirectionSelect.value = rotationDirection.toString();
+
+rotateCheckbox.addEventListener("change", () => {
+    shouldRotate = rotateCheckbox.checked;
+});
+
+rotationSpeedInput.addEventListener("input", () => {
+    rotationSpeed = parseFloat(rotationSpeedInput.value);
+});
+
+rotationDirectionSelect.addEventListener("change", () => {
+    rotationDirection = parseFloat(rotationDirectionSelect.value);
+});
+
+/**
+ * Animation Loop:
+ * - rotate the earth around itself
+ * - rotate the moon around itself
+ * - move the moon around the earth (if rotation is enabled) with a given speed
+ */
 const animate = () => {
     requestAnimationFrame(animate);
     rotateEarth();
     rotateMoon();
-    moveMoonAroundEarth();
+    const speed = rotationDirection * rotationSpeed;
+    if (shouldRotate) moveMoonAroundEarth(speed);
     render();
+
+    materials.forEach((material) => {
+        material.wireframe = shouldShowWireframe();
+    });
 };
 
 animate();
+
+// help note: accessed  from the UI through the `Help` button.
+addHelpNote({
+    title: "Scene Options",
+    description: "These options are specified for this scene:",
+    points: [
+        "Rotate: Start/Stop the moon rotating around the earth.",
+        "Rotation Speed: Adjust the speed of rotation. Default is 1.",
+        "Rotation Direction: Change the direction of rotation. Default is counter clockwise.",
+    ],
+});
