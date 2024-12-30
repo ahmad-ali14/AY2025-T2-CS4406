3e28bac87efb1b361da8d4058cff4d75f3b48abe
Ahmad Ali <aallii300300@gmail.com>
2024-12-30 21:11:19 +0000

    unit 7: give more configs to user + labels

--- src/unit7/code.ts
+++ src/unit7/code.ts
@@ -1,15 +1,9 @@
 import * as THREE from "three";
 import { createBaseScene } from "../utils/createBaseScene";
+import { createTextSprite } from "../utils/createTextSprite";
 
-// const n = window.innerHeight * 10;
-const n = 1;
-const incr = 0.01;
-// const n = window.innerHeight / 20;
-const worldEdges = {
-    x: { min: -n, max: n },
-    y: { min: -n, max: n },
-    z: { min: -n, max: n },
-};
+let n = 1;
+let incr = 0.01;
 
 const {
     camera,
@@ -22,38 +16,43 @@ const {
     sidebar,
     addHelpNote,
     ambientLight,
+    shouldShowLabels,
 } = createBaseScene({
     sceneTitle: "Unit 7: Function Graphing",
     cameraZ: n * 15,
     cameraFov: 90,
     defaultLightColor: "#fff",
     showAxes: true,
-    showGrid: true,
+    showGrid: false,
     useAmbientLight: true,
     usePointLight: false,
     gridHelperSize: n * 10,
     gridHelperDivisions: n * 10 * 10,
-    showWireframe: false,
+    showWireframe: true,
 });
 
 const parseFunction = (fn: string): ((x: number, y: number) => number) => {
     return new Function("x", "y", `return ${fn}`) as any;
 };
 
-const minX = worldEdges.x.min;
-const maxX = worldEdges.x.max;
-const minY = worldEdges.y.min;
+const planeSize = (n: number) => n * 100;
 
-const planeGeometry = new THREE.PlaneGeometry(n * 20, n * 20, 10, 10);
+const planeGeometry = new THREE.PlaneGeometry(
+    planeSize(n),
+    planeSize(n),
+    10,
+    10,
+);
 const planeMaterial = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
+    color: "#84bbfa",
     side: THREE.DoubleSide,
     wireframe: false,
+    opacity: 0.5,
 });
 
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = Math.PI / 2;
-plane.position.y = -n;
+plane.position.y = -n *2;
 scene.add(plane);
 
 const curveGeometry = new THREE.BufferGeometry();
@@ -64,44 +63,42 @@ const curveMaterial = new THREE.MeshPhongMaterial({
 const curve = new THREE.Mesh(curveGeometry, curveMaterial);
 scene.add(curve);
 
-const plotFunction1 = (fnString: string) => {
+const plotFunction = (fnString: string) => {
     const fn = parseFunction(fnString);
 
-    const vertices = [];
+    if (!n || !incr) {
+        alert("Please set N and Increment values.");
+        return;
+    }
+
+    // clean up the previous vectors
+    // curve.geometry.dispose();
+    curve.geometry = new THREE.BufferGeometry();
+
     const vectors: THREE.Vector3[] = [];
-    const indices: number[] = [];
+
+    const minX = -n;
+    const maxX = n;
+    const minY = -n;
+    const maxY = n;
 
     for (let x = minX; x <= maxX; x += incr) {
-        for (let y = minY; y <= worldEdges.y.max; y += incr) {
+        for (let y = minY; y <= maxY; y += incr) {
             const z = fn(x, y);
-            // // add a point with the z,y,z
-            // const geometry1 = new THREE.SphereGeometry(incr * 2, 32, 32);
-            // const material1 = new THREE.MeshPhongMaterial({
-            //     color: new THREE.Color(`hsl(${z * 10}, 100%, 50%)`),
-            //     wireframe: shouldShowWireframe(),
-            // });
-            // const sphere = new THREE.Mesh(geometry1, material1);
-            // sphere.position.set(x, y, z);
-            // scene.add(sphere);
-            vertices.push(x, y, z);
             vectors.push(new THREE.Vector3(x, y, z));
-            // indices.push(
-            //     vectors.length - 1,
-            //     // vectors.length - 1,
-            //     // vectors.length - 1,
-            // );
         }
     }
 
-    curve.geometry.setAttribute(
-        "position",
-        new THREE.Float32BufferAttribute(vertices, 3),
-    );
-    curve.geometry.setIndex(indices);
-    curve.geometry.computeVertexNormals();
-    // curve.geometry.setFromPoints(vectors);
+    console.log(vectors.length);
+    console.log("vectors", vectors);
 
-    // curve.geometry.computeBoundingSphere();
+    curve.geometry.setFromPoints(vectors);
+    plane.geometry = new THREE.PlaneGeometry(
+        planeSize(n),
+        planeSize(n),
+        10,
+        10,
+    );
 };
 
 const defaultFunctions = [
@@ -149,7 +146,7 @@ sceneOptionsDiv.innerHTML = `
                                  </div>
                                  <div class="ml-5">
                                     <code class="block bg-gray-200 text-blue-600 font-mono text-sm p-2 rounded">
-                                        ${f.fn}
+                                       z = ${f.fn}
                                     </code>
                                  </div>
                             </label>`,
@@ -159,10 +156,20 @@ sceneOptionsDiv.innerHTML = `
         <div class="flex flex-col space-y-2 text-lg">
             <label for="function" class="font-bold">Custom Function:</label>
             <div class="text-sm">Please follow Javascript notation (and not Math), similar to the options above.</div>
-            <input type="text" id="customFunction" class="p-2 border border-gray-400 rounded" />
+            <div class="flex gap-1 items-center">
+                <div>z = </div>
+                <input type="text" id="customFunction" class="p-2 border border-gray-400 rounded" />
+            </div>
             <button id="plotFunction" class="p-2 bg-blue-500 text-white rounded">Plot</button>
         </div>
-
+        <div class="flex items-center space-x-2">
+            <span class="font-bold">N:</span>
+            <input type="range" id="nInput" min="${0.1}" max="${3}" step="0.1" value="${n}" />
+        </div>
+        <div class="flex items-center space-x-2">
+            <span class="font-bold">Incr:</span>
+            <input type="range" id="incrInput" min="${0.01}" max="${1}" step="0.01" value="${incr}" />
+        </div>
     </div>
     `;
 
@@ -175,8 +182,12 @@ const customFunctionInput = document.querySelector(
 const plotFunctionButton = document.querySelector(
     "#plotFunction",
 ) as HTMLButtonElement;
+const nInput = document.querySelector("#nInput") as HTMLInputElement;
+const incrInput = document.querySelector("#incrInput") as HTMLInputElement;
 
 customFunctionInput.value = fnString;
+incrInput.value = incr.toString();
+nInput.value = n.toString();
 
 functionRadios.forEach((radio) => {
     radio.addEventListener("change", (e) => {
@@ -193,14 +204,66 @@ plotFunctionButton.addEventListener("click", () => {
     functionChanged = true;
 });
 
+nInput.addEventListener("input", (e) => {
+    setTimeout(() => {
+        n = parseFloat((e.target as HTMLInputElement).value);
+        functionChanged = true;
+    }, 1500);
+});
+
+incrInput.addEventListener("input", (e) => {
+    incr = parseFloat((e.target as HTMLInputElement).value);
+    functionChanged = true;
+});
+
 camera.position.set(n * 5, n * 5, n * 5);
 camera.lookAt(0, 0, 0);
 
+const edge = 4;
+
+const createAxisLabels = (
+    axis: "x" | "y" | "z",
+): THREE.Sprite<THREE.Object3DEventMap>[] => {
+    const labels: THREE.Sprite<THREE.Object3DEventMap>[] = [];
+    const axisName = createTextSprite(axis.toUpperCase());
+    axisName.position.set(0, 0, 0);
+    axisName.position[axis] = edge + 1;
+    labels.push(axisName);
+
+    for (let i = 0; i <= edge; i++) {
+        const label = createTextSprite(i.toString());
+        label.position.set(0, 0, 0);
+        label.position[axis] = i;
+        labels.push(label);
+    }
+    return labels;
+};
+
+const labels = [
+    ...createAxisLabels("x"),
+    ...createAxisLabels("y"),
+    ...createAxisLabels("z"),
+];
+
+labels.forEach((l) => scene.add(l));
+
 const animate = () => {
     if (functionChanged) {
-        plotFunction1(fnString);
+        plotFunction(fnString);
         functionChanged = false;
     }
+    const materials = [
+        // plane.material,
+        curve.material,
+    ];
+    materials.forEach((m) => {
+        m.wireframe = shouldShowWireframe();
+    });
+
+    labels.forEach((l) => {
+        l.visible = shouldShowLabels();
+    });
+
     requestAnimationFrame(animate);
     render();
 };
